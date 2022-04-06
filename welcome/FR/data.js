38e8241c1cfd6efdda13f3611c50701715578a4b
guidetalle <103005752+guidetalle@users.noreply.github.com>
2022-04-05 22:46:41 -0400

    Update data.js

--- welcome/FR/data.js
+++ welcome/FR/data.js
@@ -2,7 +2,7 @@ const sublinks = [
     {
       page: 'CV/Resume',
       links: [
-        { label: 'CV/Resume intéractif', icon: 'fas fa-address-card', url: 'resume.html' },
+        { label: 'CV/Resume intéractif', icon: 'fas fa-address-card', url: '/welcome/resume.html' },
         { label: 'CV/Resume (PDF)', icon: 'fas fa-address-card', url: 'cv.pdf' },
         { label: 'Certificat UDEMY', icon: 'fas fa-graduation-cap', url: 'https://www.udemy.com/certificate/UC-552f2d06-6b8f-4d29-8669-b531ec459154' },
       ],
@@ -10,7 +10,7 @@ const sublinks = [
     {
       page: 'Portfolio',
       links: [
-        { label : 'Présentation de mes projets', icon : 'fas fa-book', url : './portfolio.html'},
+        { label : 'Présentation de mes projets', icon : 'fas fa-book', url : '/welcome/portfolio.html'},
         { label: 'Catalogue de recettes de cuisine', icon: 'fas fa-book', url: '/RecipeApp/index.html' },
         { label: 'Quiz : géographie du Canada', icon: 'fas fa-book', url: '/CapitalQuiz/welcome.html' },
       ],
